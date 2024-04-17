3115291101ffdb620cb36c3a3ca1b6a422b9120f
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2024-04-17 18:54:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_streamlit_app():
     try:
-        subprocess.run(["streamlit", "run", "--server.port", "8000", "app_ui.py"], check=True)
+        subprocess.run(["streamlit", "hello", "--server.address", "0.0.0.0", "--server.port", "8000"], check=True)
     except subprocess.CalledProcessError as e:
         print("Error:", e)
 
